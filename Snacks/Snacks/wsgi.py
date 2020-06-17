2dc7c23b0f2e38cb5208985c4d87c64a7f4b3b8b
Ryan Premi <rpremi@calpoly.edu>
2020-06-16 21:14:46 -0700

    check

---
+++ Snacks/Snacks/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Snacks project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Snacks.settings')
+
+application = get_wsgi_application()
