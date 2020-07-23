d8fa93ffc2711880c10dc099778386ea994fc767
Ryan Premi <rpremi@calpoly.edu>
2020-07-22 20:14:51 -0700

    sqlite 3.0 -> postgresql!

--- Snacks/Snacks/settings.py
+++ Snacks/Snacks/settings.py
@@ -75,13 +75,40 @@ WSGI_APPLICATION = 'Snacks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'mytest',
+        'PASSWORD': '',
+        'HOST' : 'localhost',
+        'PORT' : '',
+    }
+}
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
