1282e5161d124eeeb6ed6877107167a0e1475a06
Ryan Premi <rpremi@calpoly.edu>
2020-07-24 19:36:36 -0700

    added html templates

--- Main/views.py
+++ Main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def index(request):
 	return render(request, 'accounts/main.html')
 
 def snacks(request):
-	return HttpResponse("This is the snacks page.")
+	snacks = Snack.objects.all()
+	return render(request, 'accounts/snacks.html', {'snacks': snacks})
