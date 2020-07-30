3a62de9bb699fde000e8a786d39ac608faaec582
Hakata-jpn <47042245+Hakata-jpn@users.noreply.github.com>
2020-07-30 13:20:53 -0700

    Added Adobe fonts to login page, adjusted positioning of components on the page

--- test-login-page/App.js
+++ test-login-page/App.js
@@ -3,6 +3,16 @@ import React from 'react';
 import { TouchableOpacity, StyleSheet, Text, View, Alert } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
+// Adobe font embed
+(function(d) {
+  var config = {
+    kitId: 'sdk5xhw',
+    scriptTimeout: 3000,
+    async: true
+    },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
 // const buttonColor = 'gray';
 
 export default function App() {
@@ -22,7 +32,7 @@ export default function App() {
         </TextInput>
 
         {/* Forgot Password Button */}
-        <View style={styles.textInline}> 
+        <View style={styles.textInline1}> 
             <TouchableOpacity>
               <Text style={styles.linkTextBoldPass}>Forgot your Password?</Text>
             </TouchableOpacity>
@@ -34,7 +44,7 @@ export default function App() {
         </TouchableOpacity>
 
         {/* Registration Button */}
-          <View style={styles.textInline}> 
+          <View style={styles.textInline2}> 
             <TouchableOpacity>
               <Text h3 style={styles.linkText}>Don't have an account? <Text style={styles.linkTextBold}>Register</Text></Text> 
             </TouchableOpacity>
@@ -60,15 +70,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     display: 'flex',
     alignItems: "center",
-    paddingBottom: '40%',
+    paddingBottom: '10%',
   },
 
   header: {
     height: '10%',
-    fontSize: 30,
     justifyContent: "center",
     textAlign: "center",
     paddingBottom: '15%',
+    fontSize: 36,
+    fontFamily: 'europa',
+    fontWeight: '700',
   },
 
   textbox: {
@@ -79,6 +91,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 12,
     fontSize: 18,
+    fontFamily: 'europa',
   },
 
   button: {
@@ -88,14 +101,26 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     alignItems: "center",
     padding: 20,
-    marginTop: '10%',
+    marginTop: 0,
   },
 
   buttonText: {
     color: 'white',
+    fontWeight: '700',
+    fontFamily: 'europa',
   },
 
-  textInline: {
+  textInline1: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: '5%',
+    paddingTop: 0,
+    marginLeft: 'auto',
+    marginRight: '3%',
+  },
+
+  textInline2: {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -108,17 +133,20 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 16,
     marginTop: 0,
+    fontFamily: 'europa',
   },
 
   linkTextBoldPass: {
     color: 'black',
     fontSize: 16,
     paddingTop: 0,
+    fontFamily: 'europa',
   },
 
   linkText: {
     color: 'black',
     fontSize: 16,
+    fontFamily: 'europa',
   },
 
 
